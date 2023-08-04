590ee6f8b52eec7d137c23a0e74ce3f86d6bf985
Dor <dorraba100@gmail.com>
2023-08-04 10:57:15 +0300

    new commit

---
+++ src/gameEngien/rule/action/increase/action.java
@@ -0,0 +1,27 @@
+package gameEngien.rule.action.increase;
+
+import gameEngien.entity.Entity;
+import gameEngien.property.propertyInterface.PropertyInterface;
+import gameEngien.rule.action.actionInterface.ActionInterface;
+
+public class action implements ActionInterface {
+    @Override
+    public String getEntityName() {
+        return null;
+    }
+
+    @Override
+    public String getPropertyName() {
+        return null;
+    }
+
+    @Override
+    public void activateAction(Entity entity) {
+
+    }
+
+    @Override
+    public boolean setValues(PropertyInterface v1, PropertyInterface v2) {
+        return false;
+    }
+}
