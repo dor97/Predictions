1ea2f8cfe444c9552c94b531e0fb1a5fbfb63d82
oriavitan <oriavitan888@gmail.com>
2023-08-18 11:47:53 +0300

    New Changes

--- src/UI/ConsoleUI/Main.java
+++ src/UI/ConsoleUI/Main.java
@@ -1,24 +1,9 @@
 package UI.ConsoleUI;
-
-import Engine.Engine;
-
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
 public class Main {
 
     public static void main(String[] args) {
         ConsoleUI consoleUI = new ConsoleUI();
         consoleUI.start();
-//        String s = "da";
-//        Integer x = 5;
-//        Float y = 5f;
-//        Float z = (float)x;
-//
-//        if(y == x){
-//            System.out.println("equals");
-//        }else{
-//            System.out.println("not equals");
-//        }
     }
 }
 
